39f4f7371566b2b79f9ac395610c9327da37ddd5
mohsin-code <mohsinshah3171@gmail.com>
2022-07-25 02:11:35 +0500

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast';
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
 
 export default MyApp
